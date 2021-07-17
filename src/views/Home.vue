b9113e57ca1b04526b7e7757e2f789397ba6b94a
rezky08 <rezky.setiawan85@gmail.com>
2021-07-17 07:03:25 +0700

    - change to default layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <default-layout>
+  <div>
     <el-row :gutter="20" class="mx-auto sm:w-full md:w-1/2 gutter" justify="center">
       <el-col :lg="8" :md="12" :sm="12" :xs="12">
         <card-counter :title="$tc('menu.customer', { count: 2 })" :content="289" />
@@ -20,15 +20,15 @@
         <card-button :title="$tc('menu.invitation')" :icon="WeddingInvitation1" />
       </el-col>
     </el-row>
-  </default-layout>
+  </div>
 </template>
 <script>
 import CardButton from "../components/cards/card-button.vue";
 import CardCounter from "../components/cards/card-counter.vue";
-import DefaultLayout from "../layouts/default-layout.vue";
+
 import { WeddingIcon, WeddingInvitation1 } from "../assets/icons/wedding";
 export default {
-  components: { DefaultLayout, CardCounter, CardButton },
+  components: { CardCounter, CardButton },
   data() {
     return {
       WeddingIcon,
